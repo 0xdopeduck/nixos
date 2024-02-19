8e5b4649b27765372f943e65f67a7f6ae2e43221
0xdopeduck <0xdopeduck@example.com>
2024-02-19 18:09:06 +0530

    Setup a command reference tool

--- home-manager/.config/home-manager/modules/cybersec.nix
+++ home-manager/.config/home-manager/modules/cybersec.nix
@@ -7,7 +7,7 @@
 
 	#Network
   	nmap
-	proxychains
+	#proxychains
 	sshuttle
 	
 	#Wordlist
@@ -35,7 +35,7 @@
 	#password
 	thc-hydra
 	john
-	hashcat
+	#hashcat
 	hash-identifier
 
 	#C2
@@ -45,6 +45,20 @@
 
   home.file = {
   	"wordlist/seclists".source = "${pkgs.seclists}/share/wordlists/seclists";
+	".commands.txt" = { text = ''
+nmap -A -T4 -v -p		#Top 1000 ports TCP All scan
+nmap -T4 -v -p-			#All ports TCP open port scan
+sudo nmap -v -sU		#Top 1000 ports UDP Scan
+ffuf -ac -H "Host: FUZZ.	#VHOST Fuzzing
+ffuf -ac			#General Web Fuzzing CMD
+		'';
+			    executable = false;};
+			    # Command to search and copy the above commands to clipboard
+			    # cat .commands.txt | cut -d"#" -f1 | tr -d '\t\n' | xclip --selection clipboard
+
+  };
+  home.shellAliases = {
+  	cmd = "cat $HOME/.commands.txt| fzf |cut -d'#' -f1 |  tr -d '\t\n' | xclip -i -selection clipboard";
   };
 
 

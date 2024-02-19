810c837281ad4653a66e9080c526f0f1b8be8a2b
0xdopeduck <0xdopeduck@example.com>
2024-02-19 18:06:50 +0530

    Added a python virtual environment plugin

--- home-manager/.config/home-manager/programs/zsh.nix
+++ home-manager/.config/home-manager/programs/zsh.nix
@@ -8,15 +8,10 @@
 	programs.zsh.history.share = true;
 	programs.zsh.history.size = 1000;
 	programs.zsh.enableCompletion = true;
-	programs.zsh.dirHashes = {
-		
-		htb = "$HOME/HTB";
-		seclists = "/usr/share/seclists";
-
-	};
 	programs.zsh.antidote.enable = true;
 	programs.zsh.antidote.plugins = [
 		"zdharma-continuum/fast-syntax-highlighting"
+		"MichaelAquilina/zsh-autoswitch-virtualenv"
 	];
 		
 }

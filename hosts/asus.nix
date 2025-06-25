129476fd73dd25cf3b5de05739f71e43033550c1
0xdopeduck <0xdopeduck@example.com>
2025-06-25 22:10:07 +0530

    Added support for asus keys and improved nividia

--- hosts/asus.nix
+++ hosts/asus.nix
@@ -13,6 +13,13 @@
       enable = true;
       enableUserService = true;
   };
+  
+  services.udev.extraHwdb = ''
+      evdev:name:*:dmi:bvn*:bvr*:bd*:svnASUS*:pn*:*
+       KEYBOARD_KEY_ff31007c=f20    # fixes mic mute button
+       KEYBOARD_KEY_ff3100b2=home   # Set fn+LeftArrow as Home
+       KEYBOARD_KEY_ff3100b3=end    # Set fn+RightArrow as End
+    '';
 
   # Add supergfxd to manager the GPU
   services.supergfxd.enable = true;
@@ -33,7 +40,8 @@
   ];
 
   # boot.kernelParams = [ "amdgpu.dc=0" ];
-  boot.kernelParams = [ "quiet" ];
+  # boot.kernelParams = [ "mitigations=off" "quiet" "nvidia-drm.modeset=1" ];
+  boot.kernelParams = [ "quiet" "nvidia-drm.modeset=1" ];
 
   # Setup Latest opensource Nvidia Drivers
   services.xserver.videoDrivers = [ "nvidia" ];
@@ -48,9 +56,12 @@
     # Hybrid GPU switching
     prime = {
       offload.enable = true;
+      offload.enableOffloadCmd = true;
       nvidiaBusId = "PCI:1:0:0";
       amdgpuBusId = "PCI:4:0:0";
     };
+
+    package = config.boot.kernelPackages.nvidiaPackages.stable;
 
 
     # For proper battery management and suspend
